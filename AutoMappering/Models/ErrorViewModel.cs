afc60afd9c282ee44421caa7cadcd73171488f26
Ali Ranjbaran <aliii4elemnt@gmail.com>
2023-04-18 08:08:34 +0330

    initioal project

---
+++ AutoMappering/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutoMappering.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
